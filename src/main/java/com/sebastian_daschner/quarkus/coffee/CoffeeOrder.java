f82196c131d390e189632981ebe1b0312d11d9a7
Sebastian Daschner <git@sebastian-daschner.de>
2023-04-17 13:04:27 +0200

    JSON-B mapping added

--- src/main/java/com/sebastian_daschner/quarkus/coffee/CoffeeOrder.java
+++ src/main/java/com/sebastian_daschner/quarkus/coffee/CoffeeOrder.java
@@ -1,4 +1,38 @@
 package com.sebastian_daschner.quarkus.coffee;
 
+import javax.json.bind.annotation.JsonbTransient;
+import java.util.UUID;
+
 public class CoffeeOrder {
+
+//    @JsonbTransient
+    public UUID id;
+
+    //    @JsonbProperty("coffeeType")
+    private String type;
+
+    public CoffeeOrder() {
+        id = UUID.randomUUID();
+    }
+
+    public CoffeeOrder(String type) {
+        id = UUID.randomUUID();
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "CoffeeOrder{" +
+               "id=" + id +
+               ", type='" + type + '\'' +
+               '}';
+    }
 }
--- src/main/java/com/sebastian_daschner/quarkus/coffee/CoffeeOrdersResource.java
+++ src/main/java/com/sebastian_daschner/quarkus/coffee/CoffeeOrdersResource.java
@@ -1,2 +1,41 @@
-package com.sebastian_daschner.quarkus.coffee;public class CoffeeOrderResource {
+package com.sebastian_daschner.quarkus.coffee;
+
+import javax.inject.Inject;
+import javax.json.bind.Jsonb;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("orders")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class CoffeeOrdersResource {
+
+    @Inject
+    Jsonb jsonb;
+
+    @GET
+    public List<CoffeeOrder> orders() {
+        return List.of(new CoffeeOrder("Espresso"), new CoffeeOrder("Cappuccino"));
+    }
+
+    @GET
+    @Path("123")
+    public CoffeeOrder order() {
+        CoffeeOrder order = new CoffeeOrder("Espresso");
+
+        String json = jsonb.toJson(order);
+        System.out.println("json = " + json);
+
+        return order;
+    }
+
+    @POST
+    public Response create(CoffeeOrder order) {
+        System.out.println("order " + order + " created");
+
+        return Response.accepted().build();
+    }
+
 }
